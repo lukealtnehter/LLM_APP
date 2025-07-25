fb81f7bcc369a824b10bbc3a4085b22a3e279a1a
yinxi <yin.xi@utsouthwestern.edu>
2025-07-25 17:31:45 -0500

    Create install_all_packages.R

---
+++ install_all_packages.R
@@ -0,0 +1,28 @@
+#install all packages here
+
+required_packages <- unique(c(
+  "shiny",
+  "DT",
+  "tidyverse",
+  "readxl",
+  "shinycssloaders",
+  "dplyr",
+  "shinythemes",
+  "htmltools",
+  "gtsummary",
+  "gt",
+  "ollamar",
+  "shiny", "jsonlite", "shinyjs", "jsonvalidate",
+  "readxl", "writexl", "tidyverse", "ollamar",
+  "httr2", "arsenal"
+))
+
+# Function to check and install packages
+
+install_if_missing <- function(package) {
+  if (!requireNamespace(package, quietly = TRUE)) {
+    install.packages(package)
+  }
+}
+# Install all required packages
+invisible(lapply(required_packages, install_if_missing))
